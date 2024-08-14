62dff5e4789d7174e51a9225df4a16fdfb81fa0a
Jack-Andersen <62236370+Jack-Andersen@users.noreply.github.com>
2024-08-14 07:56:16 +0200

    Finished Find-Pi-to-the-Nth-Digit

---
+++ Numbers/Find-PI-to-the-Nth-Digit/Find-PI-to-the-Nth-Digit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Enter the number of decimal places for PI: ");
+		int n = int.Parse(Console.ReadLine());
+
+		if (n < 0 || n > 15)
+		{
+			Console.WriteLine("Please enter a number between 0 and 15.");
+		}
+		else
+		{
+			// Use the Math.PI constant and format it to N decimal places
+			string piToNthDigit = Math.PI.ToString("F" + n);
+			Console.WriteLine($"PI to {n} decimal places: {piToNthDigit}");
+		}
+	}
+}
